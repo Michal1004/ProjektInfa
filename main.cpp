e9f3f18df82c844a481ee907c4e197d8a718cd47
Michal1004 <michal.baginski10@gmail.com>
2025-01-03 12:29:02 +0100

    Podstawowy main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-cout <<"Hello World!";
-  cout <<"Test";
+int main()
+{
+    char akcja;
+    string login, haslo, imie, nazwisko;
+
+    cout << "Witaj w Bibliotece!" << endl;
+
+
+    do {
+        cout << "Masz konto? Zaloguj sie po kliknieciu Z" << endl;
+        cout << "Nie masz konta? Zarejestruj sie po kliknieciu R" << endl;
+        cin >> akcja;
+        if (akcja == 'Z' || akcja == 'z') {
+            cout << "Login: ";
+            cin >> login;
+            cout << "Haslo: ";
+            cin >> haslo;
+            cout << "Zalogowano pomyslnie!" << endl;
+            break;
+        }
+        else if (akcja == 'R' || akcja == 'r') {
+            cout << "Imie: ";
+            cin >> imie;
+            cout << "Nazwisko: ";
+            cin >> nazwisko;
+            cout << "Nazwa uzytkownika/login: ";
+            cin >> login;
+            cout << "Podaj haslo do konta: ";
+            cin >> haslo;
+            cout << "Konto zostało utworzone!" << endl;
+            break;
+        }
+        else {
+            cout << "Nieprawidlowe zapytanie, wprowadz jeszcze raz.\n";
+        }
+    } while (true);
+
+    return 0;
 }
