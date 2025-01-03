2052639faa78b41ad23ae09f027556e945dfb6cf
Michal1004 <michal.baginski10@gmail.com>
2025-01-03 18:37:53 +0100

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,4 +1,15 @@
 #include "user.h"
+#include "baza.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
+User :: User(string Imie, string Nazwisko, string Login, string Haslo):
+imie(Imie), nazwisko(Nazwisko), login(Login), haslo(Haslo){}
+
+void User::pokazHistorie() const {
+
+}
+void User::setHaslo() {
+
+}
