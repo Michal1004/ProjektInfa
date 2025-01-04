51ce6e1cccaea884db68c4d96cc5291024ac69bf
Michal1004 <michal.baginski10@gmail.com>
2025-01-04 20:31:48 +0100

    settery I funkcja do pokazania loginu imienia i nazwiska

--- user.cpp
+++ user.cpp
@@ -9,9 +9,19 @@ imie(Imie), nazwisko(Nazwisko), login(Login), haslo(Haslo){}
 void User::pokazHistorie() const {
 
 }
-void User::setHaslo() {
-
+void User::setLogin(string Login) {
+    login = Login;
 }
+void User::setHaslo(string Haslo) {
+    haslo = Haslo;
+}
+void User::setNazwisko(string Nazwisko) {
+    nazwisko = Nazwisko;
+}
+void User::setImie(string Imie) {
+    imie = Imie;
+}
+
 bool User::sprawdzDane(string wpisanyLogin, string wpisaneHaslo) const {
     if(wpisanyLogin == login && wpisaneHaslo == haslo) {
         return true;
@@ -20,4 +30,9 @@ bool User::sprawdzDane(string wpisanyLogin, string wpisaneHaslo) const {
         cout<<"Niepoprawne hasło i/lub login"<<endl;
         return false;
     }
+}
+void User::pokazDane() const {
+    cout << "Imię: " << imie << endl;
+    cout << "Nazwisko: " << nazwisko << endl;
+    cout << "Login: " << login << endl;
 }
