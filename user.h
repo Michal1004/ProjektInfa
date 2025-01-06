aee0101abd1931653e706544a4a324fb334c109d
Michal1004 <michal.baginski10@gmail.com>
2025-01-06 16:30:44 +0100

    historia

--- user.h
+++ user.h
@@ -1,16 +1,15 @@
 #pragma once
 #include <string>
+#include <vector>
 using namespace std;
 
-class User{
+class User {
 private:
     string imie;
     string nazwisko;
     string haslo;
     string login;
-
-protected:
-    int ilosc; //chodzi o to ile ksiazek wypozyczyl
+    vector<string> historiaWypozyczen;
 
 public:
     User(string Imie, string Nazwisko, string Login, string Haslo);
@@ -18,8 +17,8 @@ public:
     void setLogin(string Login);
     void setNazwisko(string Nazwisko);
     void setImie(string Imie);
-    void pokazHistorie() const;
-    void wypozyczKsiazki()const;
     bool sprawdzDane(string wpisanyLogin, string wpisaneHaslo) const;
     void pokazDane() const;
+    void dodajDoHistorii(const string& tytul);
 };
+
