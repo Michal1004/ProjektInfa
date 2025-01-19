ce028144a812c58c6464f572624144a8a2e9ad04
Asnd123 <kuba.klyszczynski@o2.pl>
2025-01-19 18:50:13 +0100

    Update user.h

--- user.h
+++ user.h
@@ -9,6 +9,7 @@ private:
     string nazwisko;
     string haslo;
     string login;
+    string rola;
     vector<string> historiaWypozyczen;
 
 public:
@@ -17,10 +18,14 @@ public:
     void setLogin(string Login);
     void setNazwisko(string Nazwisko);
     void setImie(string Imie);
+    void setRola(string Rola);
+
     bool sprawdzDane(string wpisanyLogin, string wpisaneHaslo) const;
     void pokazDane() const;
     void dodajDoHistorii(const string& tytul);
     void pokazHistorie() const;
+
+    string getRola() const;
 
     static void wczytajUzytkownikowZPliku(vector<User>& uzytkownicy, const string& nazwaPliku);
     static void zapiszUzytkownikowDoPliku(const vector<User>& uzytkownicy, const string& nazwaPliku);
