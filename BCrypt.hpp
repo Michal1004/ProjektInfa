c2d55aa2f2ba6f6267788105c6376cb8f4ae1438
Michal1004 <michal.baginski10@gmail.com>
2025-01-27 19:56:17 +0100

    Update BCrypt.hpp

--- BCrypt.hpp
+++ BCrypt.hpp
@@ -1,10 +1,1 @@
-#pragma once
 
-#include <string>
-
-class BCrypt {
-public:
-    static std::string generateHash(const std::string& password);
-
-    static bool validatePassword(const std::string& password, const std::string& hash);
-};
