f489fa97b8ac0f3f84121febe0632ccc11093cf5
Asnd123 <kuba.klyszczynski@o2.pl>
2025-01-27 21:38:21 +0100

    Update baza.h

--- baza.h
+++ baza.h
@@ -1,32 +1,38 @@
 #pragma once
-#include <vector>
 #include <string>
-#include <unordered_map>
+#include <vector>
+
+#include <functional>
+#include <sstream>
+
 using namespace std;
 
-class Ksiazka {
-public:
-    string tytul;
-    string autor;
-    bool wypozyczona = false;
-    string wypozyczajacy = "";
-};
-
-class Biblioteka {
+class User {
 private:
-    unordered_map<string, pair<string, int>> ksiazki;
-    unordered_map<string, vector<string>> historiaWypozyczen;
+    string imie;
+    string nazwisko;
+    string haslo;
+    string login;
+    string rola;
+    vector<string> historiaWypozyczen;
 
 public:
-    Biblioteka(const string& nazwaPliku);
-    Biblioteka(){}
+    User(string Imie, string Nazwisko, string Login, string Haslo, string Rola);
+    void setHaslo(string Haslo);
+    void setLogin(string Login);
+    void setNazwisko(string Nazwisko);
+    void setImie(string Imie);
+    void setRola(string Rola);
 
-    void dodajKsiazke(const Ksiazka& nowaKsiazka);
-    void wyswietlKsiazki() const;
-    bool wypozyczKsiazke(const string& tytul, const string& login);
-    bool zwracanieKsiazki(const string& tytul, const string& login);
-    void wyswietlHistorieWypozyczen(const string& login) const;
+    bool sprawdzDane(string wpisanyLogin, string wpisaneHaslo) const;
+    void pokazDane() const;
+    void dodajDoHistorii(string& tytul);
+    void pokazHistorie() const;
 
-    void wczytajKsiazkiZPliku(const string& nazwaPliku);
-    void zapiszKsiazkiDoPliku(const string& nazwaPliku) const;
+    string getRola() const;
+
+    static void wczytajUzytkownikowZPliku(vector<User>& uzytkownicy, const string& nazwaPliku);
+    static void zapiszUzytkownikowDoPliku(const vector<User>& uzytkownicy, const string& nazwaPliku);
+
+    static string hashHaslo(const string& haslo);
 };
